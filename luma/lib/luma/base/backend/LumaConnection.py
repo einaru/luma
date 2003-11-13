c15ba6e4b830da380b0a93244cf520854369e830
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2003-11-13 13:50:48 +0000

    Added async search functions. UI busy handling added to all functions.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@45 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/backend/LumaConnection.py
+++ luma/lib/luma/base/backend/LumaConnection.py
@@ -7,6 +7,7 @@
 ###########################################################################
 
 import ldap
+from qt import *
 
 class LumaConnection(object):
     
@@ -14,16 +15,20 @@ class LumaConnection(object):
         self.server = None
         if not (serverMeta == None):
             self.server = serverMeta
+        
+        self.mainWin = qApp.mainWidget()
             
 ###############################################################################
 
     def search_s(self, base="", scope=ldap.SCOPE_BASE, filter="(objectClass=*)", attrList=None):
         searchResult = None
         
+        self.mainWin.set_busy(1)
+        
         try:
             ldapServerObject = ldap.open(self.server.host, self.server.port)
             ldapServerObject.protocol_version = ldap.VERSION3
-            if self.server.tls:
+            if int(self.server.tls) == 1:
                 ldapServerObject.start_tls_s()
             if len(self.server.bindDN) > 0:
                 ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
@@ -31,8 +36,51 @@ class LumaConnection(object):
             searchResult = ldapServerObject.search_s(base, scope, filter, attrList)
             if len(self.server.bindDN) > 0:
                 ldapServerObject.unbind()
+            self.mainWin.set_busy(0)
         except ldap.LDAPError, e:
             print "Error during LDAP request"
             print "Reason: " + str(e)
+            self.mainWin.set_busy(0)
             
         return searchResult
+
+###############################################################################
+
+    def search(self, base="", scope=ldap.SCOPE_BASE, filter="(objectClass=*)", attrList=None, attrsonly=0):
+        searchResult = []
+        
+        self.mainWin.set_busy(1)
+        
+        try:
+            ldapServerObject = ldap.open(self.server.host, self.server.port)
+            ldapServerObject.protocol_version = ldap.VERSION3
+            if int(self.server.tls) == 1:
+                ldapServerObject.start_tls_s()
+            if len(self.server.bindDN) > 0:
+                ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
+            
+            resultId = ldapServerObject.search(base, scope, filter, attrList)
+            
+            while 1:
+                self.mainWin.update_ui()
+
+                result_type, result_data = ldapServerObject.result(resultId, 0)
+                if (result_data == []):
+                    break
+                else:
+                    if result_type == ldap.RES_SEARCH_ENTRY:
+                        for x in result_data:
+                            searchResult.append(x)
+                
+            if len(self.server.bindDN) > 0:
+                ldapServerObject.unbind()
+            self.mainWin.set_busy(0)
+        except ldap.LDAPError, e:
+            print "Error during LDAP request"
+            print "Reason: " + str(e)
+            self.mainWin.set_busy(0)
+            
+        if len(searchResult) == 0:
+            return None
+        else:
+            return searchResult
