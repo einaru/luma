8f1951767d757066e8044caf3405df00533328ca
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-01-03 17:05:17 +0000

    Removed dependecy from mx-datetime module. Now we don't need this module any more *hooray* :) QDate is used instead. I don't know why I have overlooked this.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@98 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/utils/backend/DateHelper.py
+++ luma/lib/luma/base/utils/backend/DateHelper.py
@@ -8,54 +8,53 @@
 #
 ###########################################################################
 
-
-from mx.DateTime import *
+from qt import *
 
 class DateHelper(object):
     """A class for doing some date calculations.
     """
+    unixBirth = None
 
 ###############################################################################
 
     def __init__(self):
         # setting the date for the birth of unix
-        self.unixBirth = Date(1970, 1, 1)
+        self.unixBirth = QDate (1970, 1, 1)
         
 ###############################################################################
 
-    def is_valid_date(self, year=1970, month=1, day=1):
+    def is_valid_date (self, year=1970, month=1, day=1):
         """Test  if a given date is valid. Default date is the birth of unix.
         
         If the date is valid 1 is returned, else 0.
         """
-        
-        try:
-            dummyDate = Date(year, month, day)
+        dummyDate = QDate (year, month, day)
+        if dummyDate.isValid ():
             return 1
-        except mx.DateTime.RangeError:
+        else:
             return 0
             
 ###############################################################################
 
-    def date_to_unix(self, year, month, day):
+    def date_to_unix (self, year, month, day):
         """ Calculates the days since the birth of unix until the given date.
         
         The result is an integer. If the given date is not valid, return None.
         """
         
-        if self.is_valid_date(year, month, day):
-            tmpDate = Date(year, month, day)
-            diff = tmpDate - self.unixBirth
-            return int(diff.days)
+        if self.is_valid_date (year, month, day):
+            tmpDate = QDate (year, month, day)
+            diff = self.unixBirth.daysTo (tmpDate)
+            return diff
             
         
 ###############################################################################
 
-    def dateduration_to_unix(self, days):
+    def dateduration_to_unix (self, days):
         """Converts the current date plus 'days' into an integer which represents the days since 
         the birth of unix. 
         """
     
-        curDate = now()
-        tmpDays = curDate - self.unixBirth
-        return int(tmpDays.days) + days
+        curDate = QDate ()
+        tmpDays = self.unixBirth.daysTo (curDate.currentDate ())
+        return tmpDays + days
