ef983168a6ecc34ec57de65e76a79599d15b620d
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-02-04 23:56:03 +0000

    Fixed some issues with chekcing version strings.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@136 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/install.py
+++ luma/install.py
@@ -18,52 +18,31 @@ import py_compile
 
 prefixDir = ""
 
+
 def doImportCheck():
-    myError = 0
     try:
         import ldap
         vString = "2.0.0pre13"
-        if ldap.__version__ >= vString:
-            print "\tGood: python-ldap (>= " + vString + ") installed."
-        else:
-            print "\tBad: Installed version of python-ldap is out of date."
-            print "\t     At least version " + vString + " is needed"
-            print "\t     Currently version " + ldap.__version__ + " is available.\n"
-            myError = 1
+        print "python-ldap is installed..."
+        print "\tInstalled Version: " + ldap.__version__
+        print "\tNeeded Version: " + vString
+        print ""
     except ImportError:
-        print """\tERROR: python-ldap not installed!!!
-\tYou can get the module here: http://python-ldap.sourceforge.net
+        print """ERROR: python-ldap not installed!!!
+You can get the module here: http://python-ldap.sourceforge.net
 """
-        myError = 1
 
     try:
         import qt
         vString = "3.7"
-        if qt.PYQT_VERSION_STR >= vString:
-            print "\tGood: PyQt (>= " + vString + ") installed."
-        else:
-            print "\tBad: Installed version of PyQt is out of date."
-            print "\t     At least version " + vString + " is needed"
-            print "\t     Currently version " + qt.PYQT_VERSION_STR + " is available.\n"
-            myError = 1
+        print "PyQt is installed..."
+        print "\tInstalled Version: " + qt.PYQT_VERSION_STR
+        print "\tNeeded Version: " + vString
+        print ""
     except ImportError:
-        print """\n\tERROR: PyQt not installed!!!
-\tYou can get the module here: http://www.riverbankcomputing.co.uk/pyqt
+        print """\nERROR: PyQt not installed!!!
+You can get the module here: http://www.riverbankcomputing.co.uk/pyqt
 """
-        myError = 1
-        
-    try:
-        import mx.DateTime
-        print "\tGood: Egenix mx package installed."
-    except ImportError:
-        print """\n\tWARNING: Egenix mx package not installed!!!
-\tYou can get the module here: http://www.egenix.com/
-\tNOTE: This package is only needed for the "Massive User Creation"- and the
-\t"Admin Utilities"-Plugin.
-"""
-
-    if myError:
-        sys.exit(1)
 
 def doChecks():
     global prefixDir
@@ -139,7 +118,7 @@ anywhere in the console.
 
 def printHelp():
     helpString = """Install options:
- --prefix=PATH \t install path (e.g. /usr/local)"""
+ --prefix=PATH \t install path (e.g. /usr/local)\n"""
     print helpString
     sys.exit(1)
     
@@ -157,7 +136,7 @@ def doCompile():
 
 
 print "LUMA 1.2 (C) 2003 Wido Depping\n"
-print "Check for preinstalled modules:"
+print "Check for preinstalled modules:\n"
 doImportCheck()
 print ""
 
