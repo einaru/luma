579d9efe09eee12c9bf0ca48dc6bb78db6fe8fae
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-02-08 21:32:44 +0000

    Two new utility functions.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@147 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/utils/__init__.py
+++ luma/lib/luma/base/utils/__init__.py
@@ -1,1 +1,17 @@
+from qt import *
 
+def lumaStringEncode(tmpString):
+    tmpString = unicode(tmpString).encode('utf-8')
+    tmpString = tmpString.replace("\\", "\\\\")
+    tmpString = tmpString.replace(",", r"\\\1")
+        
+    return tmpString
+        
+###############################################################################
+
+def lumaStringDecode(tmpString):
+    tmpString = unicode(tmpString)
+    tmpString = tmpString.replace(r"\\\1", ",")
+    tmpString = tmpString.replace("\\\\", "\\")
+        
+    return tmpString
