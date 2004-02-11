777ba2212a1764b07c577eb3c725ac3a94d437b0
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-02-11 20:12:14 +0000

    Fixes and new method.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@162 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/utils/__init__.py
+++ luma/lib/luma/base/utils/__init__.py
@@ -1,7 +1,9 @@
 from qt import *
+import re
+import base64
 
 def lumaStringEncode(tmpString):
-    tmpString = unicode(tmpString).encode('utf-8')
+    #tmpString = unicode(tmpString).encode('utf-8')
     tmpString = tmpString.replace("\\", "\\\\")
     tmpString = tmpString.replace(",", r"\\\1")
         
@@ -15,3 +17,20 @@ def lumaStringDecode(tmpString):
     tmpString = tmpString.replace("\\\\", "\\")
         
     return tmpString
+
+###############################################################################
+
+def isBinaryAttribute(tmpString):
+    BINARY_PATTERN = '(^(\000|\n|\r| |:|<)|[\000\n\r\200-\377]+|[ ]+$)'
+    binaryPattern = re.compile(BINARY_PATTERN)
+    
+    if binaryPattern.search(tmpString) == None:
+        return 0
+    else:
+        return 1
+        
+###############################################################################
+
+def encodeBase64(tmpString):
+    return base64.encodestring(tmpString)
+    
