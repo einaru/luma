27fdd6e2b177b19c524e223368b6737fe605109a
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-03-31 14:57:54 +0000

    Rearranged some of the code. You have to bind/unbind manually now.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@172 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/backend/LumaConnection.py
+++ luma/lib/luma/base/backend/LumaConnection.py
@@ -11,78 +11,81 @@
 import ldap
 
 import environment
+from base.backend.ServerObject import ServerObject
 
+class LumaConnectionException(Exception):
+    """Will be raised is no proper server object is passed to the constructor.
+    """
+    pass
+
+    
 
 class LumaConnection(object):
+    """ This class is a wrapper around the ldap functions. It is provided to access ldap 
+    data easier.
+    
+    Parameter is a ServerObject which contains all meta information for accessing servers.
+    """
     
     def __init__(self, serverMeta=None):
-        self.server = None
+        # Throw exception if no ServerObject is passed.
+        if not (isinstance(serverMeta, ServerObject)):
+            exceptionString = "Expected ServerObject type. Passed object was "
+            exceptionString = exceptionString + str(type(serverMeta))
+            raise LumaConnectionException, exceptionString
         
-        if not (serverMeta == None):
-            self.server = serverMeta
+        self.server = serverMeta
         
-            
+        # This ldap object will be assigned in the methods.
+        # This way we have better control over bind, unbind and open sockets.
+        self.ldapServerObject = None
+        
 ###############################################################################
 
     def search_s(self, base="", scope=ldap.SCOPE_BASE, filter="(objectClass=*)", attrList=None):
-        searchResult = None
+        """Synchronous search.
+        """
         
-        environment.set_busy(1)
+        searchResult = None
+        environment.setBusy(1)
         
         try:
-            ldapServerObject = ldap.open(self.server.host, self.server.port)
-            ldapServerObject.protocol_version = ldap.VERSION3
-            if self.server.tls == 1:
-                ldapServerObject.start_tls_s()
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
-                
-            searchResult = ldapServerObject.search_s(base, scope, filter, attrList)
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.unbind()
+            searchResult = self.ldapServerObject.search_s(base, scope, filter, attrList)
         except ldap.LDAPError, e:
             print "Error during LDAP request"
             print "Reason: " + str(e)
             
-        environment.set_busy(0)
-        
+        environment.setBusy(0)
         return searchResult
 
 ###############################################################################
 
     def search(self, base="", scope=ldap.SCOPE_BASE, filter="(objectClass=*)", attrList=None, attrsonly=0):
+        """Aynchronous search.
+        """
+    
         searchResult = []
-        
-        environment.set_busy(1)
+        environment.setBusy(1)
         
         try:
-            ldapServerObject = ldap.open(self.server.host, self.server.port)
-            ldapServerObject.protocol_version = ldap.VERSION3
-            if self.server.tls == 1:
-                ldapServerObject.start_tls_s()
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
-            
-            resultId = ldapServerObject.search(base, scope, filter, attrList, attrsonly)
+            resultId = self.ldapServerObject.search(base, scope, filter, attrList, attrsonly)
             
             while 1:
-                environment.update_ui()
-
-                result_type, result_data = ldapServerObject.result(resultId, 0)
+                environment.updateUI()
+                result_type, result_data = self.ldapServerObject.result(resultId, 0)
+                
                 if (result_data == []):
                     break
                 else:
                     if result_type == ldap.RES_SEARCH_ENTRY:
                         for x in result_data:
                             searchResult.append(x)
-                
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.unbind()
+
         except ldap.LDAPError, e:
             print "Error during LDAP request"
             print "Reason: " + str(e)
-            
-        environment.set_busy(0)
+         
+        environment.setBusy(0)
             
         if len(searchResult) == 0:
             return None
@@ -92,91 +95,90 @@ class LumaConnection(object):
 ###############################################################################
 
     def delete_s(self, dnDelete=None):
+        """Synchronous delete.
+        """
+        
         if dnDelete == None:
             return
             
         result = None
-        
-        environment.set_busy(1)
+        environment.setBusy(1)
         
         try:
-            ldapServerObject = ldap.open(self.server.host, self.server.port)
-            ldapServerObject.protocol_version = ldap.VERSION3
-            
-            if self.server.tls == 1:
-                ldapServerObject.start_tls_s()
-                
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
-                
-            ldapServerObject.delete_s(dnDelete)
-            
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.unbind()
-                
-            environment.set_busy(0)
+            self.ldapServerObject.delete_s(dnDelete)
+            environment.setBusy(0)
             return 1
         except ldap.LDAPError, e:
             print "Error during LDAP request"
             print "Reason: " + str(e)
-            environment.set_busy(0)
+            environment.setBusy(0)
             return 0
             
 ###############################################################################
 
     def modify_s(self, dn, modlist=None):
+        """Synchronous modify.
+        """
+        
         if modlist == None:
             return 0
             
-        environment.set_busy(1)
-        
+        environment.setBusy(1)
         try:
-            ldapServerObject = ldap.open(self.server.host, self.server.port)
-            ldapServerObject.protocol_version = ldap.VERSION3
-            
-            if self.server.tls == 1:
-                ldapServerObject.start_tls_s()
-                
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
-                
-            ldapServerObject.modify_s(dn, modlist)
-            
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.unbind()
-                
-            environment.set_busy(0)
+            self.ldapServerObject.modify_s(dn, modlist)
+            environment.setBusy(0)
             return 1
         except ldap.LDAPError, e:
             print "Error during LDAP request"
             print "Reason: " + str(e)
-            environment.set_busy(0)
+            environment.setBusy(0)
             return 0
 
 ###############################################################################
 
     def add_s(self, dn, modlist):
-        environment.set_busy(1)
+        """Synchronous add.
+        """
+        
+        environment.setBusy(1)
+        
         
         try:
-            ldapServerObject = ldap.open(self.server.host, self.server.port)
-            ldapServerObject.protocol_version = ldap.VERSION3
-            
-            if self.server.tls == 1:
-                ldapServerObject.start_tls_s()
-                
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
-             
-            searchResult = ldapServerObject.add_s(dn, modlist)
-          
-            if len(self.server.bindDN) > 0:
-                ldapServerObject.unbind()
-                
-            environment.set_busy(0)
+            searchResult = self.ldapServerObject.add_s(dn, modlist)
+            environment.setBusy(0)
             return 1
         except ldap.LDAPError, e:
             print "Error during LDAP request"
             print "Reason: " + str(e)
-            environment.set_busy(0)
+            environment.setBusy(0)
             return 0
+
+###############################################################################
+
+    def bind(self):
+        """Bind to server.
+        """
+        
+        try:
+            self.ldapServerObject = ldap.open(self.server.host, self.server.port)
+            self.ldapServerObject.protocol_version = ldap.VERSION3
+            if self.server.tls:
+                self.ldapServerObject.start_tls_s()
+            if not(self.server.bindAnon):
+                self.ldapServerObject.simple_bind_s(self.server.bindDN,self.server.bindPassword)
+        except ldap.LDAPError, e:
+            print "Error during LDAP request"
+            print "Reason: " + str(e)
+            
+###############################################################################
+
+    def unbind(self):
+        """Unbind from server.
+        """
+        
+        try:
+            if not(self.server.bindAnon):
+                self.ldapServerObject.unbind()
+        except ldap.LDAPError, e:
+            print "Error during LDAP request"
+            print "Reason: " + str(e)
