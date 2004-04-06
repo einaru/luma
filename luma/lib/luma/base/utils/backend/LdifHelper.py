d5b5f4f04e56031055662f4dfba7219984c50542
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-04-06 13:35:47 +0000

    Initial import.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@219 92dc13f9-bf33-0410-9015-9d03373cc0ef

---
+++ luma/lib/luma/base/utils/backend/LdifHelper.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+#    Copyright (C) 2004 by Wido Depping
+#    <widod@users.sourceforge.net>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+
+import base64
+
+from base.utils import isBinaryAttribute
+from base.utils.backend.ObjectClassAttributeInfo import ObjectClassAttributeInfo
+
+class LdifHelper(object):
+
+    def __init__(self, serverName):
+        self.SERVERNAME = serverName
+        
+        self.SCHEMAMETA = ObjectClassAttributeInfo(self.SERVERNAME)
+        
+        
+        
+###############################################################################
+
+    def convertToLdif(self, data):
+        tmpList = []
+        
+        if data == None:
+            data = []
+            
+        for a in data:
+            tmpDN = a[0]
+            if isBinaryAttribute(a[0]):
+                tmpDN = base64.encodestring(tmpDN)
+                tmpList.append("dn:: " + tmpDN)
+            else:
+                tmpList.append("dn: " + tmpDN + "\n")
+            for x in a[1].keys():
+                for y in a[1][x]:
+                    if isBinaryAttribute(y) or self.SCHEMAMETA.isBinary(x) :
+                        tmpList.append(x + ":: " + base64.encodestring(y))
+                    else:
+                        tmpList.append(x + ": " + y + "\n")
+
+            tmpList.append("\n")
+        return "".join(tmpList)
