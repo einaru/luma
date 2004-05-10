8972beca5d282fbfc9a9af7ca135d177098da635
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-05-10 13:09:09 +0000

    Added pre- and postprocessing.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@241 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/plugins/mass_creation_plugin/__init__.py
+++ luma/lib/luma/plugins/mass_creation_plugin/__init__.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
 
+###########################################################################
+#    Copyright (C) 2003 by Wido Depping
+#    <widod@users.sourceforge.net>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+
+from base.backend.ServerObject import ServerObject
+
+def preProcess(serverMeta, objectValues):
+    pass
+    
+###############################################################################
+
+def postProcess(serverMeta, objectValues):
+    pass
