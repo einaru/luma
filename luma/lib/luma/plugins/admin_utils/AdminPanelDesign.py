a49c5e17cb06d63f1c6928a8857597678ade8780
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-05-10 13:06:37 +0000

    Recompile from pyuic.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@238 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/plugins/admin_utils/AdminPanelDesign.py
+++ luma/lib/luma/plugins/admin_utils/AdminPanelDesign.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/wido/src/luma/lib/luma/plugins/admin_utils/AdminPanelDesign.ui'
 #
-# Created: Mon Apr 5 21:56:38 2004
+# Created: Mon Apr 26 16:00:28 2004
 #      by: The PyQt User Interface Compiler (pyuic) 3.11
 #
 # WARNING! All changes made in this file will be lost!
