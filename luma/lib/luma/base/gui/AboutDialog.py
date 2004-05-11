6fab15b185d816f8d76f67b4277715ac5903d249
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-05-11 21:51:54 +0000

    Updated version info.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@251 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/gui/AboutDialog.py
+++ luma/lib/luma/base/gui/AboutDialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/wido/src/luma/lib/luma/base/gui/AboutDialog.ui'
 #
-# Created: Mon Apr 26 16:00:24 2004
+# Created: Mon May 10 17:49:46 2004
 #      by: The PyQt User Interface Compiler (pyuic) 3.11
 #
 # WARNING! All changes made in this file will be lost!
@@ -79,7 +79,7 @@ class AboutDialog(QDialog):
 
     def languageChange(self):
         self.setCaption(self.__tr("About Luma"))
-        self.textLabel5.setText(self.__tr("<font size=\"+2\"><b>Luma 1.2 </b></font>"))
+        self.textLabel5.setText(self.__tr("<font size=\"+2\"><b>Luma 1.3pre4 </b></font>"))
         self.textLabel4.setText(self.__tr("<p align=\"center\">LDAP management made easy.<br><br>\n"
 "(c) 2003, Wido Depping<br><br>\n"
 "http://luma.sourceforge.net\n"
