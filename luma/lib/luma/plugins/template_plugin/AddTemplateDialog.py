3764d1ba0be00aa82814b3190f20b9687e824d54
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-09-28 02:38:34 +0000

    New dialog for adding templates.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@349 92dc13f9-bf33-0410-9015-9d03373cc0ef

---
+++ luma/lib/luma/plugins/template_plugin/AddTemplateDialog.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from qt import *
+import os.path
+from string import strip
+
+from plugins.template_plugin.AddTemplateDialogDesign import AddTemplateDialogDesign
+from base.backend.ServerObject import ServerObject
+from base.backend.ServerList import ServerList
+import environment
+
+
+class AddTemplateDialog(AddTemplateDialogDesign):
+
+    def __init__(self,parent = None,name = None,modal = 0,fl = 0):
+        AddTemplateDialogDesign.__init__(self,parent,name,modal,fl)
+        
+        self.templateList = []
+        
+        tmpFile  = os.path.join(environment.lumaInstallationPrefix, "share", "luma", "icons", "secure.png")
+        securePixmap = QPixmap(tmpFile)
+
+        serverListObject = ServerList()
+        serverListObject.readServerList()
+        serverList = serverListObject.SERVERLIST
+        
+        if not (serverList == None):
+            tmpList = []
+            for x in serverList:
+                tmpList.append(x.name)
+            tmpList.sort()
+            
+            for x in tmpList:
+                tmpObject = serverListObject.getServerObject(x)
+                if tmpObject.tls:
+                    self.serverBox.insertItem(securePixmap, tmpObject.name)
+                else:
+                    self.serverBox.insertItem(tmpObject.name)
+                    
+###############################################################################
+
+    def valuesChanged(self):
+        name = strip(unicode(self.nameEdit.text()))
+        if len(name) > 0:
+            if name in self.templateList:
+                self.statusLabel.setText(self.trUtf8("A template with this name already exists."))
+            else:
+                self.okButton.setEnabled(True)
+                self.statusLabel.setText(None)
+        else:
+            self.okButton.setEnabled(False)
+            self.statusLabel.setText(self.trUtf8("Please supply a template name."))
+            
+            
+            
+            
