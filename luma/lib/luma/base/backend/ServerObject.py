0e95efe47d66d58e95cad1a19ec9d44f89bcb172
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2004-12-01 20:29:35 +0000

    Removed unneccessary code. Also added new variables autoBase and currentBase.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@458 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/backend/ServerObject.py
+++ luma/lib/luma/base/backend/ServerObject.py
@@ -31,24 +31,25 @@ class ServerObject(object):
     
     """
     
-    __slots__ = (u'nameP', u'hostP', u'portP', u'bindAnonP', u'baseDNP', u'bindDNP', 
-        u'bindPasswordP', u'tlsP', u'authMethodP')
-    
     authentificationMethods = [u"Simple", u"SASL Plain", u"SASL CRAM-MD5", 
         u"SASL DIGEST-MD5", u"SASL Login", u"SASL GSSAPI"]
 
 ###############################################################################
 
     def __init__(self):
-        self.nameP = u""
-        self.hostP = u""
-        self.portP = 389
-        self.bindAnonP = 0
-        self.baseDNP = u""
-        self.bindDNP = u""
-        self.bindPasswordP = u""
-        self.tlsP = 0
-        self.authMethodP = u"Simple"
+        self.name = u""
+        self.host = u""
+        self.port = 389
+        self.bindAnon = True
+        self.autoBase = True
+        self.baseDN = []
+        self.bindDN = u""
+        self.bindPassword = u""
+        self.tls = False
+        self.authMethod = u"Simple"
+        
+        # This value will only set during runtime
+        self.currentBase = u""
         
 ###############################################################################
 
@@ -64,7 +65,7 @@ class ServerObject(object):
         finalString.append(unicode("\nBind anonymously: "))
         finalString.append(unicode(self.bindAnon))
         finalString.append(unicode("\nBase DN: "))
-        finalString.append(unicode(self.baseDN))
+        map(lambda x: finalString.append(unicode(x) + u", "), self.baseDN)
         finalString.append(unicode("\nBind DN: "))
         finalString.append(unicode(self.bindDN))
         finalString.append(unicode("\nBind Password: "))
@@ -76,137 +77,3 @@ class ServerObject(object):
         finalString.append(unicode("\n"))
 
         return "".join(finalString)
-        
-###############################################################################
-
-
-    def __setName(self, name):
-        if isinstance(name, unicode):
-            self.nameP = name
-        else:
-            raise AttributeError, "Expected an unicode string."
-            
-    def __getName(self):
-        return self.nameP
-            
-    
-    name = property(__getName, __setName, None, "Name of the server.")
-        
-###############################################################################
-
-    def __setHost(self, host):
-        if isinstance(host, unicode):
-            self.hostP = host
-        else:
-            raise AttributeError, "Expected an unicode string."
-            
-    def __getHost(self):
-        return self.hostP
-            
-    
-    host = property(__getHost, __setHost, None, "Host address of the server.")
-    
-###############################################################################
-
-    def __setPort(self, port):
-        if not(isinstance(port, int)):
-            raise AttributeError, "Expected an integer."
-            
-        if (port < 0) or (port > 65535):
-            raise ValueError, "Port not in range 0-65535"
-            
-        self.portP = port
-            
-            
-    def __getPort(self):
-        return self.portP
-            
-    
-    port = property(__getPort, __setPort, None, "Port of the server.")
-    
-###############################################################################
-
-    def __setBindAnon(self, value):
-        if isinstance(value, bool):
-            self.bindAnonP = value
-        else:
-            raise AttributeError, "Expected a boolean."
-            
-    def __getBindAnon(self):
-        return self.bindAnonP
-            
-    
-    bindAnon = property(__getBindAnon, __setBindAnon, None, "Port of the server.")
-    
-###############################################################################
-
-    def __setBaseDN(self, value):
-        if isinstance(value, unicode):
-            self.baseDNP = value
-        else:
-            raise AttributeError, "Expected an unicode string."
-            
-    def __getBaseDN(self):
-        return self.baseDNP
-            
-    
-    baseDN = property(__getBaseDN, __setBaseDN, None, "Base DN of the server.")
-    
-###############################################################################
-
-    def __setBindDN(self, value):
-        if isinstance(value, unicode):
-            self.bindDNP = value
-        else:
-            raise AttributeError, "Expected an unicode string."
-            
-    def __getBindDN(self):
-        return self.bindDNP
-            
-    
-    bindDN = property(__getBindDN, __setBindDN, None, "Base DN of the server.")
-    
-###############################################################################
-
-    def __setBindPassword(self, value):
-        if isinstance(value, unicode):
-            self.bindPasswordP = value
-        else:
-            raise AttributeError, "Expected an unicode string."
-            
-    def __getBindPassword(self):
-        return self.bindPasswordP
-            
-    
-    bindPassword = property(__getBindPassword, __setBindPassword, None, "Bind password of the server.")
-
-###############################################################################
-
-    def __setTls(self, value):
-        if isinstance(value, bool):
-            self.tlsP= value
-        else:
-            raise AttributeError, "Expected a boolean."
-            
-    def __getTls(self):
-        return self.tlsP
-            
-    
-    tls = property(__getTls, __setTls, None, "Connect with/without tls to the server.")
-
-###############################################################################
-
-    def __setMethod(self, value):
-        if isinstance(value, unicode):
-            self.authMethodP= value
-        else:
-            raise AttributeError, "Expected an unicode string."
-            
-        if not(value in self.authentificationMethods):
-            raise AttributeError, "Not a supported method."
-            
-    def __getMethod(self):
-        return self.authMethodP
-            
-    
-    authMethod = property(__getMethod, __setMethod, None, "Authentification method.")
