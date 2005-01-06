30c6d8fc9b82d633603aaa3f116d2d165c479234
widod <widod@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2005-01-06 22:08:35 +0000

    Added some utility functions.
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@498 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/utils/__init__.py
+++ luma/lib/luma/base/utils/__init__.py
@@ -46,3 +46,69 @@ def isBinaryAttribute(tmpString):
 def encodeBase64(tmpString):
     return base64.encodestring(tmpString)
     
+###############################################################################
+
+def stripSpecialChars(tmpString):
+    tmpString = tmpString.replace(r'\5C', '\\')
+    tmpString = tmpString.replace(r'\2C', ',')
+    tmpString = tmpString.replace(r'\3D', '=')
+    tmpString = tmpString.replace(r'\2B', '+')
+        
+    return tmpString
+        
+###############################################################################
+    
+def escapeSpecialChars(tmpString):
+    tmpList = tmpString.split('=')
+    
+    if 2 == len(tmpList):
+        attribute = tmpList[0]
+        value = tmpList[1]
+        value = value.replace('\\', r'\5C')
+        value = value.replace(',', r'\2C')
+        value = value.replace('=', r'\3D')
+        value = value.replace('+', r'\2B')
+        
+        tmpString = attribute + '=' + value
+        
+    return tmpString
+    
+###############################################################################
+
+def explodeDN(tmpString):
+    """ Function for spliting the dn into it's parts.
+    """
+        
+    tmpList = tmpString.split(',')
+        
+    tokenList = []
+    for x in xrange(0, len(tmpList)):
+        value = tmpList[x]
+        if "=" in value:
+            tokenList.append(value)
+        else:
+            if len(tokenList) > 0:
+                tokenList[-1] = tokenList[-1] + ',' + value
+                    
+    return tokenList
+
+###############################################################################
+
+def getSortedDnList(tmpList):
+    """ Returns a sorted list for distinguished names.
+    
+    The higher the object in the tree, the more it will be at the beginning 
+    of the sorted list. Leaves should be at the end of the list.
+    """
+    
+    tmpList.sort(__dnCompare)
+    
+    return tmpList
+    
+###############################################################################
+
+def __dnCompare(firstDN, secondDN):
+    firstList = explodeDN(firstDN)
+    secondList = explodeDN(secondDN)
+    
+    return cmp(len(firstList), len(secondList))
