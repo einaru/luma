695db24ee544cd5c934ed9b49143204f001f4683
vegarwe <vegarwe@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2007-07-04 21:50:10 +0000

    Fixed gridlayout
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@733 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/utils/gui/PromptPasswordDialog.py
+++ luma/lib/luma/base/utils/gui/PromptPasswordDialog.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'PromptPasswordDialog.ui'
+# Form implementation generated from reading ui file 'base/utils/gui/PromptPasswordDialog.ui'
 #
-# Created: Thu Jun 1 23:07:47 2006
-#      by: The PyQt User Interface Compiler (pyuic) 3.15
+# Created: Wed Jul 4 19:54:16 2007
+#      by: The PyQt User Interface Compiler (pyuic) 3.16
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -19,44 +19,45 @@ class PromptPasswordDialog(QDialog):
             self.setName("PromptPasswordDialog")
 
 
+        PromptPasswordDialogLayout = QGridLayout(self,1,1,11,6,"PromptPasswordDialogLayout")
 
-        LayoutWidget = QWidget(self,"layout1")
-        LayoutWidget.setGeometry(QRect(60,10,300,60))
-        layout1 = QGridLayout(LayoutWidget,1,1,11,6,"layout1")
+        layout1 = QGridLayout(None,1,1,0,6,"layout1")
 
-        self.passwordEdit = QLineEdit(LayoutWidget,"passwordEdit")
+        self.passwordEdit = QLineEdit(self,"passwordEdit")
         self.passwordEdit.setEchoMode(QLineEdit.Password)
 
         layout1.addWidget(self.passwordEdit,1,1)
 
-        self.textLabel4 = QLabel(LayoutWidget,"textLabel4")
+        self.textLabel4 = QLabel(self,"textLabel4")
         self.textLabel4.setAlignment(QLabel.AlignVCenter)
 
         layout1.addWidget(self.textLabel4,1,0)
 
-        self.textLabel2 = QLabel(LayoutWidget,"textLabel2")
+        self.textLabel2 = QLabel(self,"textLabel2")
         self.textLabel2.setAlignment(QLabel.WordBreak | QLabel.AlignVCenter)
 
         layout1.addMultiCellWidget(self.textLabel2,0,0,0,1)
 
-        LayoutWidget_2 = QWidget(self,"layout2")
-        LayoutWidget_2.setGeometry(QRect(10,100,350,40))
-        layout2 = QGridLayout(LayoutWidget_2,1,1,11,6,"layout2")
+        PromptPasswordDialogLayout.addLayout(layout1,0,0)
 
-        self.okButton = QPushButton(LayoutWidget_2,"okButton")
+        layout2 = QGridLayout(None,1,1,0,6,"layout2")
+
+        self.okButton = QPushButton(self,"okButton")
         self.okButton.setDefault(1)
 
         layout2.addWidget(self.okButton,0,1)
 
-        self.cancelButton = QPushButton(LayoutWidget_2,"cancelButton")
+        self.cancelButton = QPushButton(self,"cancelButton")
 
         layout2.addWidget(self.cancelButton,0,2)
         spacer5 = QSpacerItem(281,21,QSizePolicy.Expanding,QSizePolicy.Minimum)
         layout2.addItem(spacer5,0,0)
 
+        PromptPasswordDialogLayout.addLayout(layout2,1,0)
+
         self.languageChange()
 
-        self.resize(QSize(371,153).expandedTo(self.minimumSizeHint()))
+        self.resize(QSize(371,131).expandedTo(self.minimumSizeHint()))
         self.clearWState(Qt.WState_Polished)
 
         self.connect(self.okButton,SIGNAL("clicked()"),self.accept)
@@ -68,9 +69,9 @@ class PromptPasswordDialog(QDialog):
         self.textLabel4.setText(self.__tr("Password:"))
         self.textLabel2.setText(self.__tr("<b>Please enter a password to bind with.</b>"))
         self.okButton.setText(self.__tr("&OK"))
-        self.okButton.setAccel(self.__tr("Alt+O"))
+        self.okButton.setAccel(QKeySequence(self.__tr("Alt+O")))
         self.cancelButton.setText(self.__tr("&Cancel"))
-        self.cancelButton.setAccel(self.__tr("Alt+C"))
+        self.cancelButton.setAccel(QKeySequence(self.__tr("Alt+C")))
 
 
     def __tr(self,s,c = None):
