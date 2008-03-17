3896d3ed91ae661fffad5413516404c9bc8a2c86
vegarwe <vegarwe@92dc13f9-bf33-0410-9015-9d03373cc0ef>
2008-03-17 17:54:10 +0000

    Qt4 migration. Remove all Q3Support widgets
    git-svn-id: https://luma.svn.sourceforge.net/svnroot/luma/trunk@795 92dc13f9-bf33-0410-9015-9d03373cc0ef

--- luma/lib/luma/base/utils/gui/LoggerWidgetDesign.py
+++ luma/lib/luma/base/utils/gui/LoggerWidgetDesign.py
@@ -1,107 +1,79 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/wido/src/luma/lib/luma/base/utils/gui/LoggerWidgetDesign.ui'
+# Form implementation generated from reading ui file 'lib/luma/base/utils/gui/LoggerWidgetDesign.ui'
 #
-# Created: Wed Aug 17 15:23:52 2005
-#      by: The PyQt User Interface Compiler (pyuic) 3.14.1
+# Created: Mon Mar 17 13:43:58 2008
+#      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
 
+from PyQt4 import QtCore, QtGui
 
-import sys
-from PyQt4.QtGui import *
+class Ui_LoggerWidgetDesign(object):
+    def setupUi(self, LoggerWidgetDesign):
+        LoggerWidgetDesign.setObjectName("LoggerWidgetDesign")
+        LoggerWidgetDesign.resize(QtCore.QSize(QtCore.QRect(0,0,516,217).size()).expandedTo(LoggerWidgetDesign.minimumSizeHint()))
 
-image0_data = \
-    "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\x00\x00\x00\x0d" \
-    "\x49\x48\x44\x52\x00\x00\x00\x10\x00\x00\x00\x10" \
-    "\x08\x06\x00\x00\x00\x1f\xf3\xff\x61\x00\x00\x00" \
-    "\xb1\x49\x44\x41\x54\x38\x8d\x95\x93\x4b\x0e\xc3" \
-    "\x20\x0c\x44\x9f\xab\xde\x0b\x7a\xb2\x90\x93\x61" \
-    "\x4e\x36\x5d\x90\x1f\x69\x43\xc8\x48\xec\xfc\xc6" \
-    "\x63\x5b\x98\xa4\xc8\x03\x95\x42\x8e\xd1\x90\xf4" \
-    "\x01\x78\x3d\x85\xdd\x01\x12\x66\x96\x01\x6c\x34" \
-    "\xc1\x0e\x57\xa5\x34\x57\xa3\x11\x83\x33\xbc\x9b" \
-    "\xd8\xfd\x08\xd7\xf0\x0c\xdc\xec\xa0\x0f\x27\x24" \
-    "\x7d\x2e\x0d\x46\xe0\xeb\x04\xa5\xe4\x10\x60\x9a" \
-    "\xfa\x30\x00\x92\x62\xf3\xdc\xe5\x20\x07\xad\x4a" \
-    "\x49\x82\xa4\x5a\xde\xd6\xb7\x57\x28\x25\x97\x18" \
-    "\x9b\xae\x41\xc2\xec\x4f\xe7\x45\xef\x1e\x0c\x30" \
-    "\x9b\xad\x49\x7f\xe0\x7d\x07\x17\xb0\x03\xa9\x03" \
-    "\x03\x98\xdc\xb5\x85\x38\x98\x8c\xc0\x35\x41\x08" \
-    "\x5b\x41\x58\xee\x36\x0a\x57\x03\xe0\x68\xf2\x04" \
-    "\x86\xd3\x67\x9a\xcd\xf2\x13\x18\xe0\x0b\xb8\xbc" \
-    "\xa8\x22\xac\xb4\x8f\x12\x00\x00\x00\x00\x49\x45" \
-    "\x4e\x44\xae\x42\x60\x82"
+        self.vboxlayout = QtGui.QVBoxLayout(LoggerWidgetDesign)
+        self.vboxlayout.setObjectName("vboxlayout")
 
-class LoggerWidgetDesign(QWidget):
-    def __init__(self,parent = None,name = None,fl = 0):
-        QWidget.__init__(self,parent,name,fl)
+        self.messageEdit = QtGui.QTextEdit(LoggerWidgetDesign)
+        self.messageEdit.setReadOnly(True)
+        self.messageEdit.setObjectName("messageEdit")
+        self.vboxlayout.addWidget(self.messageEdit)
 
-        self.image0 = QPixmap()
-        self.image0.loadFromData(image0_data,"PNG")
-        if not name:
-            self.setName("LoggerWidgetDesign")
+        self.hboxlayout = QtGui.QHBoxLayout()
+        self.hboxlayout.setObjectName("hboxlayout")
 
+        self.textLabel1 = QtGui.QLabel(LoggerWidgetDesign)
+        self.textLabel1.setWordWrap(False)
+        self.textLabel1.setObjectName("textLabel1")
+        self.hboxlayout.addWidget(self.textLabel1)
 
-        LoggerWidgetDesignLayout = QVBoxLayout(self,6,6,"LoggerWidgetDesignLayout")
+        self.errorBox = QtGui.QCheckBox(LoggerWidgetDesign)
+        self.errorBox.setChecked(True)
+        self.errorBox.setObjectName("errorBox")
+        self.hboxlayout.addWidget(self.errorBox)
 
-        self.messageEdit = QTextEdit(self,"messageEdit")
-        self.messageEdit.setReadOnly(1)
-        LoggerWidgetDesignLayout.addWidget(self.messageEdit)
+        self.debugBox = QtGui.QCheckBox(LoggerWidgetDesign)
+        self.debugBox.setChecked(True)
+        self.debugBox.setObjectName("debugBox")
+        self.hboxlayout.addWidget(self.debugBox)
 
-        layout2 = QHBoxLayout(None,0,6,"layout2")
+        self.infoBox = QtGui.QCheckBox(LoggerWidgetDesign)
+        self.infoBox.setChecked(True)
+        self.infoBox.setObjectName("infoBox")
+        self.hboxlayout.addWidget(self.infoBox)
 
-        self.textLabel1 = QLabel(self,"textLabel1")
-        layout2.addWidget(self.textLabel1)
+        spacerItem = QtGui.QSpacerItem(141,21,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.hboxlayout.addItem(spacerItem)
 
-        self.errorBox = QCheckBox(self,"errorBox")
-        self.errorBox.setChecked(1)
-        layout2.addWidget(self.errorBox)
+        self.clearButton = QtGui.QToolButton(LoggerWidgetDesign)
+        self.clearButton.setIcon(QtGui.QIcon("image0"))
+        self.clearButton.setAutoRaise(True)
+        self.clearButton.setObjectName("clearButton")
+        self.hboxlayout.addWidget(self.clearButton)
+        self.vboxlayout.addLayout(self.hboxlayout)
 
-        self.debugBox = QCheckBox(self,"debugBox")
-        self.debugBox.setChecked(1)
-        layout2.addWidget(self.debugBox)
+        self.retranslateUi(LoggerWidgetDesign)
+        QtCore.QObject.connect(self.clearButton,QtCore.SIGNAL("clicked()"),LoggerWidgetDesign.clearLogger)
+        QtCore.QMetaObject.connectSlotsByName(LoggerWidgetDesign)
 
-        self.infoBox = QCheckBox(self,"infoBox")
-        self.infoBox.setChecked(1)
-        layout2.addWidget(self.infoBox)
-        spacer2 = QSpacerItem(141,21,QSizePolicy.Expanding,QSizePolicy.Minimum)
-        layout2.addItem(spacer2)
-
-        self.clearButton = QToolButton(self,"clearButton")
-        self.clearButton.setIconSet(QIconSet(self.image0))
-        self.clearButton.setAutoRaise(1)
-        layout2.addWidget(self.clearButton)
-        LoggerWidgetDesignLayout.addLayout(layout2)
-
-        self.languageChange()
-
-        self.resize(QSize(516,217).expandedTo(self.minimumSizeHint()))
-        self.clearWState(Qt.WState_Polished)
-
-        self.connect(self.clearButton,SIGNAL("clicked()"),self.clearLogger)
+    def retranslateUi(self, LoggerWidgetDesign):
+        LoggerWidgetDesign.setWindowTitle(QtGui.QApplication.translate("LoggerWidgetDesign", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.textLabel1.setText(QtGui.QApplication.translate("LoggerWidgetDesign", "Display message types:", None, QtGui.QApplication.UnicodeUTF8))
+        self.errorBox.setText(QtGui.QApplication.translate("LoggerWidgetDesign", "Errors", None, QtGui.QApplication.UnicodeUTF8))
+        self.debugBox.setText(QtGui.QApplication.translate("LoggerWidgetDesign", "Debug", None, QtGui.QApplication.UnicodeUTF8))
+        self.infoBox.setText(QtGui.QApplication.translate("LoggerWidgetDesign", "Info", None, QtGui.QApplication.UnicodeUTF8))
+        self.clearButton.setToolTip(QtGui.QApplication.translate("LoggerWidgetDesign", "Clear", None, QtGui.QApplication.UnicodeUTF8))
 
 
-    def languageChange(self):
-        self.setCaption(self.__tr("Form"))
-        self.textLabel1.setText(self.__tr("Display message types:"))
-        self.errorBox.setText(self.__tr("Errors"))
-        self.debugBox.setText(self.__tr("Debug"))
-        self.infoBox.setText(self.__tr("Info"))
-        self.clearButton.setText(QString.null)
-        QToolTip.add(self.clearButton,self.__tr("Clear"))
-
-
-    def clearLogger(self):
-        print "LoggerWidgetDesign.clearLogger(): Not implemented yet"
-
-    def __tr(self,s,c = None):
-        return qApp.translate("LoggerWidgetDesign",s,c)
 
 if __name__ == "__main__":
-    a = QApplication(sys.argv)
-    QObject.connect(a,SIGNAL("lastWindowClosed()"),a,SLOT("quit()"))
-    w = LoggerWidgetDesign()
-    a.setMainWidget(w)
-    w.show()
-    a.exec_loop()
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    LoggerWidgetDesign = QtGui.QWidget()
+    ui = Ui_LoggerWidgetDesign()
+    ui.setupUi(LoggerWidgetDesign)
+    LoggerWidgetDesign.show()
+    sys.exit(app.exec_())
