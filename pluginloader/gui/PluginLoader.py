bb5b73428e49e1eb5cef3c0b38add61511f273f5
Johannes Harestad <johannes@dhcp-010164.wlan.ntnu.no>
2011-02-15 13:49:33 +0100

    dunno

--- pluginloader/gui/PluginLoader.py
+++ pluginloader/gui/PluginLoader.py
@@ -1,9 +1,54 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class PluginLoader(QDialog, Ui_PluginLoaderDesign):
-    def __init__(self):
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PluginLoaderDesign import Ui_PluginLoader
+import sys
+from random import randint
+
+class PluginLoader(QDialog, Ui_PluginLoader):
+    def __init__(self, parent = None):
+        QDialog.__init__(self, parent)
+        self.setupUi(self)
+        
+        self.model = PluginsModel()
+        self.listView.setModel(self.model)
+        
+        for n in range(10):
+            item = QStandardItem('Item %s' % randint(1, 100))
+            check = Qt.Checked if randint(0, 1) == 1 else Qt.Unchecked
+            item.setCheckState(check)
+            item.setCheckable(True)
+            item.setEditable(False)
+            self.model.appendRow(item)
+            
+        for n in range(self.model.rowCount()):
+            print self.model.item(n).checkState()
+
+    def pluginSelected(self, index):
+        #print index.row()
+        pass
+        
+    def close(self):
         pass
     
+    def closeButton(self):
+        pass
+        
+        
+class PluginsModel(QStandardItemModel):
+    def __init__(self, parent = None):
+        QStandardItemModel.__init__(self, parent)
+        
     
+def main():
+    app = QApplication(sys.argv)
+    pluginloader = PluginLoader()
+    pluginloader.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()    
 
--- pluginloader/gui/PluginLoaderDesign.py
+++ pluginloader/gui/PluginLoaderDesign.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'PluginLoaderDesign.ui'
 #
-# Created: Wed Feb  9 15:08:08 2011
+# Created: Wed Feb  9 17:17:40 2011
 #      by: PyQt4 UI code generator 4.8.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -14,23 +14,23 @@ try:
 except AttributeError:
     _fromUtf8 = lambda s: s
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName(_fromUtf8("Dialog"))
-        Dialog.resize(628, 531)
-        self.gridLayout = QtGui.QGridLayout(Dialog)
+class Ui_PluginLoader(object):
+    def setupUi(self, PluginLoader):
+        PluginLoader.setObjectName(_fromUtf8("PluginLoader"))
+        PluginLoader.resize(628, 531)
+        self.gridLayout = QtGui.QGridLayout(PluginLoader)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.label = QtGui.QLabel(Dialog)
+        self.label = QtGui.QLabel(PluginLoader)
         self.label.setObjectName(_fromUtf8("label"))
         self.gridLayout.addWidget(self.label, 2, 0, 1, 1)
-        self.line = QtGui.QFrame(Dialog)
+        self.line = QtGui.QFrame(PluginLoader)
         self.line.setFrameShape(QtGui.QFrame.HLine)
         self.line.setFrameShadow(QtGui.QFrame.Sunken)
         self.line.setObjectName(_fromUtf8("line"))
         self.gridLayout.addWidget(self.line, 3, 0, 1, 1)
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.splitter = QtGui.QSplitter(Dialog)
+        self.splitter = QtGui.QSplitter(PluginLoader)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -70,7 +70,7 @@ class Ui_Dialog(object):
         self.gridLayout_2.addWidget(self.stackedWidget, 0, 0, 1, 1)
         self.horizontalLayout.addWidget(self.splitter)
         self.gridLayout.addLayout(self.horizontalLayout, 4, 0, 1, 1)
-        self.line_2 = QtGui.QFrame(Dialog)
+        self.line_2 = QtGui.QFrame(PluginLoader)
         self.line_2.setFrameShape(QtGui.QFrame.HLine)
         self.line_2.setFrameShadow(QtGui.QFrame.Sunken)
         self.line_2.setObjectName(_fromUtf8("line_2"))
@@ -79,19 +79,21 @@ class Ui_Dialog(object):
         self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem)
-        self.pushButton = QtGui.QPushButton(Dialog)
+        self.pushButton = QtGui.QPushButton(PluginLoader)
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.horizontalLayout_2.addWidget(self.pushButton)
         self.gridLayout.addLayout(self.horizontalLayout_2, 6, 0, 1, 1)
 
-        self.retranslateUi(Dialog)
+        self.retranslateUi(PluginLoader)
         self.stackedWidget.setCurrentIndex(0)
-        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("released()")), Dialog.close)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("released()")), PluginLoader.closeButton)
+        QtCore.QObject.connect(self.listView, QtCore.SIGNAL(_fromUtf8("clicked(QModelIndex)")), PluginLoader.pluginSelected)
+        QtCore.QObject.connect(PluginLoader, QtCore.SIGNAL(_fromUtf8("finished(int)")), PluginLoader.close)
+        QtCore.QMetaObject.connectSlotsByName(PluginLoader)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("Dialog", "Available plugins:", None, QtGui.QApplication.UnicodeUTF8))
-        self.Settingsbox.setTitle(QtGui.QApplication.translate("Dialog", "GroupBox", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("Dialog", "Close", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, PluginLoader):
+        PluginLoader.setWindowTitle(QtGui.QApplication.translate("PluginLoader", "PluginLoader", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("PluginLoader", "Available plugins:", None, QtGui.QApplication.UnicodeUTF8))
+        self.Settingsbox.setTitle(QtGui.QApplication.translate("PluginLoader", "Settings:", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("PluginLoader", "Close", None, QtGui.QApplication.UnicodeUTF8))
 
