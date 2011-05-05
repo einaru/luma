d8c519e0b82ea3a2cabdb124c58e3e1ed5d77a92
Christian Forfang <cforfang@gmail.com>
2011-05-05 19:36:23 +0200

    Rm old code

--- luma/plugins/browser_plugin/BrowserView.py
+++ luma/plugins/browser_plugin/BrowserView.py
@@ -188,8 +188,6 @@ class BrowserView(QWidget):
         # This is a list of QModelIndex objects, which will be used by
         # the various context menu slots.
         # We therfore store it as a class member
-        #self.entryList.expandAll()
-        #return
         self.selection = self.entryList.selectedIndexes()
 
         openSupport = True
