4fe2c79ad4b2d3ead3b5ac5eb9870ec9a865f692
Einar Uvsløkk <einar.uvslokk@linux.com>
2011-05-16 02:14:11 +0200

    Updated the error feedback in the search plugin Moved all the Plugin* related code from util -> base.gui and base.model Renamed browser_plugin folder -> browser

--- luma/base/gui/PluginListWidget.py
+++ luma/base/gui/PluginListWidget.py
@@ -3,59 +3,61 @@
 # Copyright (c) 2011
 #     Johannes Harestad, <johannesharestad@gmail.com>
 #
-# Luma is free software; you can redistribute it and/or modify 
-# it under the terms of the GNU General Public Licence as published by 
-# the Free Software Foundation; either version 2 of the Licence, or 
+# Luma is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public Licence as published by
+# the Free Software Foundation; either version 2 of the Licence, or
 # (at your option) any later version.
 #
-# Luma is distributed in the hope that it will be useful, but 
+# Luma is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public Licence 
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public Licence
 # for more details.
 #
-# You should have received a copy of the GNU General Public Licence along 
-# with Luma; if not, write to the Free Software Foundation, Inc., 
+# You should have received a copy of the GNU General Public Licence along
+# with Luma; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 from PyQt4.QtCore import QSize
 from PyQt4.QtGui import QWidget, QListView
-from base.util.gui.PluginListWidgetDesign import Ui_pluginListWidget
-from base.util.model.PluginListWidgetModel import PluginListWidgetModel
+from ..gui.design.PluginListWidgetDesign import Ui_pluginListWidget
+from ..model.PluginListWidgetModel import PluginListWidgetModel
 
 import sys
 import logging
 
+
 class PluginListWidget(QWidget, Ui_pluginListWidget):
-    """
-    Used by mainwin to show the list of plugins that can be loaded
-    
-    Parent is given to the model, because it is going to contain not only
-    the QStandardItems but the widget for each item, that requires a parent.
+    """Used by mainwin to show the list of plugins that can be loaded.
+
+    Parent is given to the model, because it is going to contain not
+    only the ``QStandardItem`` but the widget for each item, that
+    requires a parent.
     """
     __logger = logging.getLogger(__name__)
-    
+
     def __init__(self, parent):
         QWidget.__init__(self, None)
         #dont change None to self in parent
-        
+
         self.parent = parent
         self.setupUi(self)
-        
+
         self.listView.setResizeMode(QListView.Adjust)
         self.listView.setViewMode(QListView.IconMode)
         self.listView.setModel(PluginListWidgetModel(self.parent))
-        
+
     def pluginDoubleClicked(self, index):
         if self.parent and hasattr(self.parent, "pluginSelected"):
             self.parent.pluginSelected(self.listView.model().itemFromIndex(index))
         else:
-            self.__logger.error("Cannot enter a plugin when no parent is given to PluginListWidget")       
-    
+            msg = 'Cannot open plugin. No parent given to the PluginListWidget'
+            self.__logger.error(msg)
+
     def updatePlugins(self):
-        """
-        Updates the listview with new plugins
+        """Updates the listview with new plugins
         """
         self.listView.reset()
         self.listView.setModel(PluginListWidgetModel(self.parent))
+
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
--- luma/base/model/PluginListWidgetModel.py
+++ luma/base/model/PluginListWidgetModel.py
@@ -3,28 +3,28 @@
 # Copyright (c) 2011
 #     Johannes Harestad, <johannesharestad@gmail.com>
 #
-# Luma is free software; you can redistribute it and/or modify 
-# it under the terms of the GNU General Public Licence as published by 
-# the Free Software Foundation; either version 2 of the Licence, or 
+# Luma is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public Licence as published by
+# the Free Software Foundation; either version 2 of the Licence, or
 # (at your option) any later version.
 #
-# Luma is distributed in the hope that it will be useful, but 
+# Luma is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public Licence 
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public Licence
 # for more details.
 #
-# You should have received a copy of the GNU General Public Licence along 
-# with Luma; if not, write to the Free Software Foundation, Inc., 
+# You should have received a copy of the GNU General Public Licence along
+# with Luma; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 from PyQt4.QtCore import QSettings
 from PyQt4.QtGui import QStandardItemModel, QStandardItem
-from base.backend.PluginLoader import PluginLoader
+from ..backend.PluginLoader import PluginLoader
+
 
 class PluginListWidgetModel(QStandardItemModel):
-    """
-    This model will create its own items, from the QSettings where 
-    plugins is set to "load".
+    """This model will create its own items, from the ``QSettings``
+    where plugins is set to *load*.
     """
     def __init__(self, parent = None):
         QStandardItemModel.__init__(self, parent)
@@ -32,33 +32,31 @@ class PluginListWidgetModel(QStandardItemModel):
         self._settings.beginGroup("plugins")
         self.pluginloader = PluginLoader()
         self.pluginloader.pluginsToLoad = self.__checkToLoad()
-        
+
         for plugin in self.pluginloader.plugins:
             if plugin.load == True:
                 item = QStandardItem(plugin.pluginUserString)
                 if plugin.icon:
                     item.setIcon(plugin.icon)
                 font = item.font()
-                font.setPointSize(font.pointSize() +4 )
+                font.setPointSize(font.pointSize() + 4)
                 item.setFont(font)
                 item.setEditable(False)
                 item.plugin = plugin
                 self.appendRow(item)
-    
+
     def __checkToLoad(self):
         pluginlist = []
-        
-        #When beginGroup is set to plugins, the childgroups will be each of the plugins..
+
+        # When beginGroup is set to plugins, the childgroups will be
+        # each of the plugins.
         for plugin in self._settings.childGroups():
             valueString = str(plugin) + "/load"
             value = self._settings.value(valueString, "True").toString()
             if value == "True":
                 pluginlist.append(str(plugin))
-       
+
         return pluginlist
 
-
-        
-        
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
