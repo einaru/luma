f3fd970aaa6e777541b975d0c80ebd973f6c60f2
Einar Uvsløkk <einar.uvslokk@linux.com>
2011-05-16 03:05:28 +0200

    Updated the user documentation, and updated / improved the deployment script.

--- doc/prepareDeployment.sh
+++ doc/prepareDeployment.sh
@@ -14,9 +14,48 @@ cd $basepath/source
 files_to_include=(AUTHORS ChangeLog HACKING INSTALL README)
 location="../.."
 
-for file in "${files_to_include[@]}";
-do
-	echo "Copying $file ..."
-	cp $file.rst $location/$file
-done
+showHelp()
+{
+	echo "usage: $0 [options]"
+	echo ""
+	echo "$0 is used to ensure up-to-date doc files is included in the root"
+	echo "folder prior to building a luma source distribution."
+	echo ""
+	echo "options:"
+	echo ""
+	echo "--h, --help  Display this help message."
+	echo "--html       Also generate the html documentation." 
+	echo "             This option requires sphinx to be installed"
+	echo ""
+	exit
+}
+# Generate the html docs
+generateHtmlDoc()
+{
+	cd ..
+	sphinx-build -b html source build
+	cd source
+}
+
+copyDocFilesToRepoRoot()
+{
+	for file in "${files_to_include[@]}";
+	do
+		echo "Copying $file ..."
+		cp $file.rst $location/$file
+	done
+}
+
+case $1 in
+	-h|--help)
+		showHelp
+		;;
+	--html)
+		copyDocFilesToRepoRoot
+		generateHtmlDoc
+		;;
+	*)
+		copyDocFilesToRepoRoot
+		;;
+esac
 
